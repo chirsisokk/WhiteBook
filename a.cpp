e458c5039ff60835452f12388c7fb11b076c9b16
chirs <chirsisokk@outlook.com>
2020-03-19 21:27:31 +0800

    new ignore

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int maxn = 1e5 + 7;
+const int mod = 1e9 + 7;
+
+int main() {
+    freopen("E:\\myprogram\\data.in", "r", stdin);
+    //freopen("E:\\myprogram\\data.out", "w", stdout);
+    cout << "hello world" << endl;
+    return 0;
+}
